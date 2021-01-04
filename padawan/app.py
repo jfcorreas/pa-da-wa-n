96e74a501c12b577552127dd1ff45733f4d38d0d
jfcorreas <jfcorreas@gmail.com>
2021-01-04 20:05:08 +0100

    Configured Flask and login with Logbook

--- padawan/app.py
+++ padawan/app.py
@@ -1,4 +1,14 @@
+import os
+import sys
+
 from flask import Flask
+
+import logbook
+
+from padawan.infraestructure.log_levels import LogLevel
+
+folder = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, folder)
 
 app = Flask(__name__)
 
@@ -8,5 +18,26 @@ def hello():
     return "Hello World!"
 
 
+def run_app():
+    log = configure_app()
+    debug = True
+    port = 5050
+    log.notice(f"Starting PA·DA·WA·N app in localhost:{port} (Debug: {debug})")
+    app.run(debug=debug, port=port)
+
+
+def init_logging():
+    logbook.StreamHandler(sys.stdout, level=LogLevel.info).push_application()
+    return logbook.Logger('App')
+
+
+def configure_app():
+    log = init_logging()
+    log.notice("Configuring PA·DA·WA·N Flask App: ")
+
+    return log
+
 if __name__ == '__main__':
-    app.run()
+    run_app()
+else:
+    configure_app()
