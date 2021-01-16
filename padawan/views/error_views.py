063482e1a71970e22d01fca7015840f76c1dd3be
jfcorreas <jfcorreas@gmail.com>
2021-01-16 18:57:38 +0100

    Managing error 403 view

--- padawan/views/error_views.py
+++ padawan/views/error_views.py
@@ -3,6 +3,12 @@ from flask import Blueprint, render_template
 blueprint = Blueprint('error', __name__, template_folder='templates')
 
 
+@blueprint.app_errorhandler(403)
+def error_403(_):
+    resp = render_template('errors/403.html')
+    return resp, 403
+
+
 @blueprint.app_errorhandler(404)
 def error_404(_):
     resp = render_template('errors/404.html')
